966aaadd8e2017c164796e44003dc73a7fc89532
RenKKLR <129666138+RenKKLR@users.noreply.github.com>
2023-05-31 21:10:59 +0000

    use tkinter to collect data - buy

--- main.py
+++ main.py
@@ -1,3 +1,65 @@
+from tkinter import *
+
+def buy():
+  sName = stockname.get()
+  sID = stockid.get()
+  sPrice = stockprice.get()
+  sAmount = stockamount.get()
+  sDate = stockdate.get()
+  print(sName, sID, sPrice, sAmount, sDate)
+  stockname.delete(0, END)
+  stockid.delete(0, END)
+  stockprice.delete(0, END)
+  stockamount.delete(0, END)
+  stockdate.delete(0, END)
+
+#creating the file buy
+  file = open("buy.txt", "a")
+  file.write(sName)
+  file.write(sID)
+  file.write(sPrice)
+  file.write(sAmount)
+  file.write(sDate)
+  print("Stock", sName, " has been submitted successfully")
+
+#screen design
+screen = Tk()
+screen.geometry("500x500")
+screen.title("Redi Final Project")
+heading = Label(text="Trading Performance Inquiry", bg="grey", fg="black", width="500")
+heading.pack()
+
+#label defnition and positon
+stockname_text = Label(text = "Name of Stock *")
+stockid_text = Label(text = "Stock ID *")
+stockprice_text = Label(text = "Price pro Stock *")
+stockamount_text = Label(text = "Amount *")
+stockdate_text = Label(text = "Date of Transaction (DD/MM/YYYY) *")
+stockname_text.place(x = 15, y = 70)
+stockid_text.place(x = 270, y = 70)
+stockprice_text.place(x = 15, y = 140)
+stockamount_text.place(x = 270, y = 140)
+stockdate_text.place(x = 15, y = 210)
+
+#entry definition and position
+stockname = Entry()
+stockid = Entry()
+stockprice = Entry()
+stockamount = Entry()
+stockdate = Entry()
+stockname.place(x = 15, y = 100)
+stockid.place(x = 270, y = 100)
+stockprice.place(x = 15, y = 170)
+stockamount.place(x = 270, y = 170)
+stockdate.place(x = 15, y = 240)
+
+#place button to buy or sell
+buy = Button(screen, text ="Buy", command=buy, bg = "grey")
+sell = Button(screen, text = "Sell", bg ="grey")
+buy.place(x = 15, y = 290)
+sell.place(x = 75, y = 290)
+
+
 print("Welcome to Trading Performance Inquiry!")
 
 selType = input("For a new entry of a transaction, please type E; to make an inquiry, please type I:\n")
