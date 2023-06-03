eab500d03fdfe27789717725c18921eea75ed21c
RenKKLR <129666138+RenKKLR@users.noreply.github.com>
2023-06-03 20:10:10 +0000

    add new window to make a single query - with bugs

--- main.py
+++ main.py
@@ -56,11 +56,53 @@ def sell():
         print("You cannot sell " +sName_sell+" in amount "+ sAmount_sell + ", because you didn't buy that much!")
       #else:
        #print("You cannot sell " + sName_sell +", because you didn't buy it!")
-      
+
+#create a new window to do a single query and display the result
+def create_window():
+  def submit():
+    sName_query = stockname_query.get()
+    stockname_query.delete(0, END)
+    with open('buy.csv') as buyfile:
+      buy_reader = csv.reader(buyfile)
+      next(buy_reader)
+      for row in buy_reader:
+        if sName_query in row[0]:
+          print("Buy: " + row[0], row[2], row[3])
+          #print("A toal of Buy: " + str(int(row[2])*int(row[3])) + " Euro")
+          
+          '''
+          with open('sell.csv') as sellfile:
+            sell_reader = csv.reader(sellfile)
+            next(sell_reader)
+            for row in sell_reader:
+              if sName_query in row[0]:
+                print("Sell: " + row[0], row[2], row[3])
+                print("A toal of Sell: " + str(int(row[2])*int(row[3])) + " Euro")
+              '''    
+
+        
+        #else:
+          #print("Invalid: You didn't buy Stock "+ sName_query +"!")
+    
   
   
+  window = Tk()
+  window.geometry("500x200")
+  window.title("Redi Final Project")
+  heading = Label(window, text="Single Query", bg="grey", fg="black", width="500")
+  heading.pack()
+  
+  screen.destroy()
+  
+  stockname_text = Label(window, text = "Name of Stock *")
+  stockname_text.place(x = 15, y = 50)
+  stockname_query = Entry(window,)
+  stockname_query.place(x = 15, y = 80)
 
+  submit_btn = Button(window,text ="Submit", command=submit, bg="grey")
+  submit_btn.place(x=15, y=150)
 
+  
 #screen design
 screen = Tk()
 screen.geometry("500x500")
@@ -71,7 +113,7 @@ heading.pack()
 #label defnition and positon
 stockname_text = Label(text = "Name of Stock *")
 stockid_text = Label(text = "Stock ID *")
-stockprice_text = Label(text = "Price pro Stock *")
+stockprice_text = Label(text = "Price pro Share (in Euro) *")
 stockamount_text = Label(text = "Amount *")
 stockdate_text = Label(text = "Date of Transaction (DD/MM/YYYY) *")
 stockname_text.place(x = 15, y = 70)
@@ -92,11 +134,13 @@ stockprice.place(x = 15, y = 170)
 stockamount.place(x = 270, y = 170)
 stockdate.place(x = 15, y = 240)
 
-#place button to buy or sell
+#place button to buy or sell or do a single query
 buy_btn = Button(screen, text ="Buy", command=buy, bg = "grey")
 sell_btn = Button(screen, text = "Sell", command=sell, bg ="grey")
+query_btn = Button(screen, text="Single Query ", command=create_window, bg="grey")
 buy_btn.place(x = 15, y = 290)
 sell_btn.place(x = 75, y = 290)
+query_btn.place(x=15, y=330)
 
 screen.mainloop()
 
